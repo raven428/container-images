9b38ae85d78175e8bc1eba67f24094aa55576d3d
Dmitry Sukhodoyev <raven428@gmail.com>
2025-03-02 17:33:19 +0500

    update ansible-builder image

--- sources/ansible-builder/files/build.sh
+++ sources/ansible-builder/files/build.sh
@@ -2,7 +2,6 @@
 set -ueo pipefail
 export PATH="$PYENV_ROOT/bin:$PATH"
 export DEBIAN_FRONTEND='noninteractive'
-apt-get update
 apt-get install -y git curl libreadline-dev libsqlite3-dev libbz2-dev gcc g++ make \
   zlib1g-dev libssl-dev libffi-dev liblzma-dev
 curl -sLm 11 \
@@ -12,7 +11,3 @@ eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
 pyenv install 3.11.11
 pyenv virtualenv 3.11.11 ansible
-
-# cleanup
-apt-get clean
-rm -Rf /usr/share/doc /usr/share/man /var/lib/apt/lists/* /root/.cache/pip /files
--- sources/ansible-builder/vars.sh
+++ sources/ansible-builder/vars.sh
@@ -2,8 +2,9 @@
 set -ueo pipefail
 # shellcheck disable=2034
 {
-   IMAGE_VER='000'
-   DEPENDS='ansible-ubuntu/'
+  IMAGE_VER='000'
+  DEPENDS='ansible-ubuntu/'
 }
+/usr/bin/env cp 'sources/ansible-ubuntu/files/install.sh' "sources/${TAG}/files"
 /usr/bin/env rm -rf "sources/${TAG}/_shared"
 /usr/bin/env cp -rf _shared "sources/${TAG}"
