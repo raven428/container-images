6d83edade10898c07a96bf111f17955fac72bea5
Dmitry Sukhodoyev <raven428@gmail.com>
2025-07-27 14:28:37 +0500

    enable async-check patch

--- sources/ansible-08/vars.sh
+++ sources/ansible-08/vars.sh
@@ -8,3 +8,4 @@ set -ueo pipefail
 /usr/bin/env cp -rf _shared "sources/${TAG}"
 /usr/bin/env cp -fv _shared/install/ansible/Dockerfile "sources/${TAG}"
 /usr/bin/env cp -fv _shared/install/ansible/* "sources/${TAG}/files/"
+echo 'patch -p0 </files/async-check.diff' >>"sources/${TAG}/files/build.sh"
