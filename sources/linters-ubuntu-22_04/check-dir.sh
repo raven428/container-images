084a1db0bfa6908e35bc467d5fbac7a870577a43
Dmitry Sukhodoev <raven428@gmail.com>
2026-01-28 07:59:32 +0000

    add options to lint and git ignoring

--- sources/linters-ubuntu-22_04/check-dir.sh
+++ sources/linters-ubuntu-22_04/check-dir.sh
@@ -5,14 +5,15 @@ set -ueo pipefail
 : "${CONT_NAME:="linters-generic-${USER}"}"
 : "${TERRAFORM_DIR:="tf"}"
 : "${PROM_RULES_DIR:="ansible/files/prometheus"}"
-: "${IMAGE_NAME:="ghcr.io/raven428/container-images/linters-ubuntu-22_04:latest"}"
+: "${USE_GITIGNORE:=""}"
+: "${USE_LINTIGNORE:=""}"
+: "${IMAGE_NAME:="ghcr.io/raven428/container-images/linters-ubuntu-22_04:001"}"
 DIR2CHECK="$(readlink -f "${DIR2CHECK}")"
-export TERRAFORM_DIR PROM_RULES_DIR
+export TERRAFORM_DIR PROM_RULES_DIR USE_GITIGNORE USE_LINTIGNORE
 /usr/bin/env podman run --rm \
   -i -w "${PATH2CONT}" \
   --name="${CONT_NAME}" \
   --hostname="${CONT_NAME}" \
-  -e "TERRAFORM_DIR" \
-  -e "PROM_RULES_DIR" \
+  -e "TERRAFORM_DIR" -e "PROM_RULES_DIR" -e "USE_GITIGNORE" -e "USE_LINTIGNORE" \
   -v "${DIR2CHECK}:${PATH2CONT}:ro" \
   "${IMAGE_NAME}" /check-syntax.sh
