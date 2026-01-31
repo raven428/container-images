4c9ea0ff03a4625922fc8d4121ea154f6a54cd37
Dmitry Sukhodoev <raven428@gmail.com>
2026-01-31 13:31:55 +0000

    update builder of ansible-builder image

--- sources/ansible-06/vars.sh
+++ sources/ansible-06/vars.sh
@@ -8,9 +8,9 @@ set -ueo pipefail
 /usr/bin/env cp -rf _shared "sources/${TAG}"
 /usr/bin/env cp -fv _shared/install/ansible/Dockerfile "sources/${TAG}"
 /usr/bin/env cp -fv _shared/install/ansible/* "sources/${TAG}/files/"
+echo 'patch -p0 </files/async-check.diff' >>"sources/${TAG}/files/build.sh"
 /usr/bin/env sed -i 's|^\(.*patch -p0 </files/flush-line.diff.*\)|# \1|' \
   "sources/${TAG}/files/build.sh"
-echo 'patch -p0 </files/async-check.diff' >>"sources/${TAG}/files/build.sh"
 /usr/bin/env cat "sources/${TAG}/files/async-check.diff" |
   /usr/bin/env awk \
     '/^--- .+site-packages\/ansible\/plugins\/action\/__init__/ { exit } { print }' |
--- sources/ansible-builder/files/build.sh
+++ sources/ansible-builder/files/build.sh
@@ -2,11 +2,56 @@
 set -ueo pipefail
 export PATH="$PYENV_ROOT/bin:$PATH"
 export DEBIAN_FRONTEND='noninteractive'
+
+# Python version for AppImage builds
+PYTHON_VERSION='3.11.11'
+PYTHON_SHORT="${PYTHON_VERSION%.*}"
+APPIMAGE_BUILD_DIR='/appimage-build'
+JOBS="$(nproc)"
+
+apt-get update
 apt-get install -y --no-install-recommends git curl libreadline-dev libsqlite3-dev less \
-  libbz2-dev gcc g++ make zlib1g-dev libssl-dev libffi-dev liblzma-dev patch
+  libbz2-dev gcc g++ make zlib1g-dev libssl-dev libffi-dev liblzma-dev patch patchelf \
+  libncursesw5-dev xz-utils file fuse
+rm -rf /var/lib/apt/lists/*
+
+# Install pyenv
 curl -sLm 11 \
   https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-pyenv install 3.11.11
-pyenv virtualenv 3.11.11 ansible
+pyenv install "${PYTHON_VERSION}"
+pyenv virtualenv "${PYTHON_VERSION}" ansible
+
+# Build Python for AppImage
+echo "Building Python ${PYTHON_VERSION} for AppImage..."
+mkdir -p "${APPIMAGE_BUILD_DIR}"
+cd "${APPIMAGE_BUILD_DIR}"
+
+curl -sLo "Python-${PYTHON_VERSION}.tgz" \
+  "https://www.python.org/ftp/python/${PYTHON_VERSION}/Python-${PYTHON_VERSION}.tgz"
+tar -xf "Python-${PYTHON_VERSION}.tgz"
+cd "Python-${PYTHON_VERSION}"
+
+./configure \
+  --prefix=/usr \
+  --enable-optimizations \
+  --with-ensurepip=install \
+  --enable-shared
+
+make -j"${JOBS}"
+make DESTDIR="${APPIMAGE_BUILD_DIR}/python-install" install
+
+# Download appimagetool
+echo "Downloading appimagetool..."
+cd "${APPIMAGE_BUILD_DIR}"
+curl -sLo appimagetool.AppImage \
+  https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
+chmod +x appimagetool.AppImage
+
+# Cleanup build artifacts but keep results
+rm -rf "Python-${PYTHON_VERSION}" "Python-${PYTHON_VERSION}.tgz"
+
+echo "AppImage build environment ready:"
+echo "  Python install: ${APPIMAGE_BUILD_DIR}/python-install"
+echo "  appimagetool: ${APPIMAGE_BUILD_DIR}/appimagetool.AppImage"
