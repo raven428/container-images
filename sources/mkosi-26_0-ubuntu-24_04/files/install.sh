0080756bc435bb3d36e39bf065ed30653624cf9e
Dmitry Sukhodoev <raven428@gmail.com>
2026-03-19 10:20:13 +0000

    update mkosi-26_0-ubuntu-24_04 container

--- sources/mkosi-26_0-ubuntu-24_04/files/install.sh
+++ sources/mkosi-26_0-ubuntu-24_04/files/install.sh
@@ -6,7 +6,7 @@ apt-get install -y --no-install-recommends \
   git ca-certificates python3 debootstrap systemd-container xfsprogs e2fsprogs \
   dosfstools btrfs-progs squashfs-tools debian-archive-keyring ubuntu-keyring fdisk \
   util-linux udev kmod zstd xz-utils cpio curl grub-pc-bin grub-common python3-pefile \
-  qemu-utils
+  qemu-utils gdisk qemu-system-x86 expect
 git clone --depth 1 --branch v26 https://github.com/systemd/mkosi.git /opt/mkosi
 ln -s /opt/mkosi/bin/mkosi /usr/local/bin/mkosi
 apt-get clean
