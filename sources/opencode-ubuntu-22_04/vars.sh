9cb7ad8225480928441860a2a30a2a1b50b11eb0
Dmitry Sukhodoev <raven428@gmail.com>
2026-04-02 13:10:32 +0000

    update opencode container

--- sources/opencode-ubuntu-22_04/vars.sh
+++ sources/opencode-ubuntu-22_04/vars.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ueo pipefail
-export IMAGE_VER='007'
+export IMAGE_VER='008'
 /usr/bin/env rm -rfv "sources/${TAG}/_shared"
 /usr/bin/env cp -rfv _shared "sources/${TAG}"
 /usr/bin/env cp -rfv podman.sh "sources/${TAG}/_shared"
