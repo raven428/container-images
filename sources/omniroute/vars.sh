7c2a106a363cfa1ca73af4e080d2ca1da6321d10
Dmitry Sukhodoev <raven428@gmail.com>
2026-05-31 12:23:53 +0000

    make shared checkout_upstream

--- sources/omniroute/vars.sh
+++ sources/omniroute/vars.sh
@@ -1,20 +1,12 @@
 #!/usr/bin/env bash
 set -ueo pipefail
 export IMAGE_VER='000'
-_repo_tag='v3.8.1'
 mkdir -p "sources/${TAG}/_shared"
 _upstream="sources/${TAG}/_shared/upstream"
-if /usr/bin/env git -C "${_upstream}" rev-parse --git-dir >/dev/null 2>&1; then
-  /usr/bin/env git -C "${_upstream}" reset --hard HEAD
-  /usr/bin/env git -C "${_upstream}" clean -fd
-  /usr/bin/env git -C "${_upstream}" fetch --tags
-  /usr/bin/env git -C "${_upstream}" checkout "${_repo_tag}"
-  /usr/bin/env git -C "${_upstream}" reset --hard "${_repo_tag}"
-else
-  /usr/bin/env rm -rf "${_upstream}"
-  /usr/bin/env git clone 'https://github.com/diegosouzapw/OmniRoute.git' "${_upstream}"
-  /usr/bin/env git -C "${_upstream}" checkout "${_repo_tag}"
-fi
+checkout_upstream \
+  'https://github.com/diegosouzapw/OmniRoute.git' \
+  'v3.8.1' \
+  "${_upstream}"
 # shellcheck disable=2153
 for _patch in "${IMAGE_DIR}/patches/"*.patch; do
   [[ -f "${_patch}" ]] || continue
@@ -25,4 +17,4 @@ done
 BUILD_CONTEXT_DIR="${_upstream}"
 # shellcheck disable=2034
 PODMAN_EXTRA_ARGS=(--target runner-base)
-unset _patch _upstream _repo_tag
+unset _patch _upstream
