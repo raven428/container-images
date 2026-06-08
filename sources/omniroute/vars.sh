c13984e33604cc2defcf02e5893c8feff9211d9b
Dmitry Sukhodoev <raven428@gmail.com>
2026-06-08 03:52:55 +0000

    add OmniRoute warmup patch

--- sources/omniroute/vars.sh
+++ sources/omniroute/vars.sh
@@ -7,10 +7,7 @@ set -ueo pipefail
 }
 stage_shared_assets
 _upstream="sources/${TAG}/_shared/upstream"
-checkout_upstream \
-  'https://github.com/diegosouzapw/OmniRoute.git' \
-  'v3.8.1' \
-  "${_upstream}"
+checkout_upstream 'https://github.com/diegosouzapw/OmniRoute.git' 'v3.8.1' "${_upstream}"
 # shellcheck disable=2153
 for _patch in "${IMAGE_DIR}/patches/"*.patch; do
   [[ -f "${_patch}" ]] || continue
