a8edcebca2df353e1d64f6d82ce8d27b9c6b436e
Dmitry Sukhodoev <raven428@gmail.com>
2026-06-14 13:43:47 +0000

    add snoopy-jae image

---
+++ sources/snoopy-jae/vars.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+# shellcheck disable=2034
+{
+  IMAGE_VER='000'
+  SHARED_ASSETS=()
+}
+stage_shared_assets
+_upstream="sources/${TAG}/_shared/upstream"
+checkout_upstream \
+  'https://github.com/jae-jae/searxng-mul-mcp.git' 'a3881c7' "${_upstream}"
+# shellcheck disable=2153
+for _patch in "${IMAGE_DIR}/patches/"*.patch; do
+  [[ -f "${_patch}" ]] || continue
+  echo "applying ${_patch}"
+  patch -d "${_upstream}" -p1 <"${_patch}"
+done
+# shellcheck disable=2034
+BUILD_CONTEXT_DIR="${_upstream}"
+unset _patch _upstream
