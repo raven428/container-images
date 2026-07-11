3231438da417ffc241cd8ef48e10b9e5b02a9650
Dmitry Sukhodoev <raven428@gmail.com>
2026-07-11 10:48:32 +0000

    skip latest and date tags on pull requests

--- _shared/_all/push.sh
+++ _shared/_all/push.sh
@@ -19,8 +19,10 @@ for IMAGE_DIR in "${IMAGES_DIRS[@]}"; do
   # shellcheck source=/dev/null
   source "${IMAGE_DIR}/vars.sh"
   current_date="$(/usr/bin/env date '+%Y%m%d')"
-  /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:latest"
-  /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:${current_date}"
+  if [[ "${IMAGE_VER}" != "999" ]]; then
+    /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:latest"
+    /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:${current_date}"
+  fi
   /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:${IMAGE_VER}"
   /usr/bin/env podman push "${TARGET_REGISTRY}/${TAG}:${IMAGE_VER}-${current_date}"
   /usr/bin/env podman image rm -f \
