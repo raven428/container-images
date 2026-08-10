b3076d00aa1bedde2c06e1a09ae2786b2836fdd8
Dmitry Sukhodoev <raven428@gmail.com>
2026-08-10 12:07:58 +0000

    update ubuntu linters

--- sources/linters-ubuntu-22_04/check-dir.sh
+++ sources/linters-ubuntu-22_04/check-dir.sh
@@ -7,13 +7,12 @@ set -ueo pipefail
 : "${PROM_RULES_DIR:="ansible/files/prometheus"}"
 : "${USE_GITIGNORE:=""}"
 : "${USE_LINTIGNORE:=""}"
-: "${IMAGE_NAME:="ghcr.io/raven428/container-images/linters-ubuntu-22_04:001"}"
+: "${IMAGE_NAME:="ghcr.io/raven428/container-images/linters-ubuntu-22_04:latest"}"
+: "${PODMAN_ARGS:=""}"
 DIR2CHECK="$(readlink -f "${DIR2CHECK}")"
 export TERRAFORM_DIR PROM_RULES_DIR USE_GITIGNORE USE_LINTIGNORE
-/usr/bin/env podman run --rm \
-  -i -w "${PATH2CONT}" \
-  --name="${CONT_NAME}" \
-  --hostname="${CONT_NAME}" \
-  -e "TERRAFORM_DIR" -e "PROM_RULES_DIR" -e "USE_GITIGNORE" -e "USE_LINTIGNORE" \
-  -v "${DIR2CHECK}:${PATH2CONT}:ro" \
+# shellcheck disable=2086
+/usr/bin/env podman run --rm -i -w "${PATH2CONT}" --name="${CONT_NAME}" \
+  --hostname="${CONT_NAME}" -e TERRAFORM_DIR -e PROM_RULES_DIR -e USE_GITIGNORE \
+  -e "USE_LINTIGNORE" -v "${DIR2CHECK}:${PATH2CONT}:ro" ${PODMAN_ARGS} \
   "${IMAGE_NAME}" /check-syntax.sh
